64872015c7f2018bba6c2cd78899ed4a9147a86d
ksqb0156596 <jademk@126.com>
2016-01-20 10:26:21 +0800

    轻码云

--- src/main/java/com/ulplanet/trip/controller/QingmaCallController.java
+++ src/main/java/com/ulplanet/trip/controller/QingmaCallController.java
@@ -1,8 +1,11 @@
 package com.ulplanet.trip.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.ulplanet.trip.bean.QingmaRecord;
 import com.ulplanet.trip.service.QingmaRecordService;
 import com.ulplanet.trip.util.QingmaValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -17,6 +20,7 @@ import java.util.Map;
 public class QingmaCallController{
 
 
+    Logger logger = LoggerFactory.getLogger(QingmaCallController.class);
     @Resource
     private QingmaRecordService qingmaRecordService;
     /**
@@ -26,6 +30,7 @@ public class QingmaCallController{
     @RequestMapping(value = "/callAuth",method = RequestMethod.POST)
     public Map<String,Object> callAuth(@RequestBody QingmaRecord qingmaRecord){
         Map<String,Object> map = new HashMap<>();
+        logger.error(JSON.toJSONString(qingmaRecord));
         qingmaRecord.setClientPwd(qingmaRecordService.getPwd(qingmaRecord));
         map.put("respCode", QingmaValidator.validator(qingmaRecord));
         map.put("respCode", "00000");
